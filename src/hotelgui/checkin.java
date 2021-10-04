0c4eb03a694ca73addaf7db9ba3d82aa875e8d18
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-10-05 03:39:30 +0800

    Test Commit From Netbeans to the Master Branch

--- src/hotelgui/checkin.java
+++ src/hotelgui/checkin.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hotelgui;
 
 import java.sql.*;
@@ -13,15 +8,9 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author expzak
- */
+
 public class checkin extends javax.swing.JFrame {
 
-    /**
-     * Creates new form checkin
-     */
     public checkin() {
         initComponents();
         Connect();
