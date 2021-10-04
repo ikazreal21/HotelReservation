f008915c86da610d21108b1d71d30fa2cfcd3f16
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-10-05 03:44:26 +0800

    Remove Some Unnecessary Comments

--- src/hotelgui/checkout.java
+++ src/hotelgui/checkout.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hotelgui;
 
 import java.sql.Connection;
@@ -20,15 +15,8 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author expzak
- */
 public class checkout extends javax.swing.JFrame {
 
-    /**
-     * Creates new form checkout
-     */
     public checkout() {
         initComponents();
         Connect();
--- src/hotelgui/openingform.java
+++ src/hotelgui/openingform.java
@@ -1,19 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hotelgui;
 
-/**
- *
- * @author expzak
- */
 public class openingform extends javax.swing.JFrame {
 
-    /**
-     * Creates new form openingform
-     */
     public openingform() {
         initComponents();
     }
